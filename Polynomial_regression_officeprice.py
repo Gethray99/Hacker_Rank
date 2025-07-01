0e1c34ff5e6548268aac7549b9a04c1a54ed04a2
Gethray99 <98901059+Gethray99@users.noreply.github.com>
2025-07-01 21:47:19 +0530

    complete Polynomial reg

--- Polynomial_regression_officeprice.py
+++ Polynomial_regression_officeprice.py
@@ -7,4 +7,16 @@ def convert_float(x):
 
 F,N = map(int, sys.stdin.readline().strip().split())
 train_data = [convert_float(sys.stdin.readline().strip()) for _ in range(N)]
-print(train_data)
+X_train = [i[:-1] for i in train_data]
+y_train = [i[-1] for i in train_data]
+poly = PolynomialFeatures(degree=3)
+X_poly = poly.fit_transform(X_train)
+model = LinearRegression()
+model.fit(X_poly, y_train)
+
+num_rows = int(sys.stdin.readline().strip())
+for _ in range(num_rows):
+    x_test = convert_float(sys.stdin.readline().strip())
+    x_poly = poly.transform([x_test])
+    y_pred = model.predict(x_poly)
+    print(f"{y_pred[0]:.2f}")  
